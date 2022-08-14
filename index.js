20f95c8e9bd3d266f5a3965417d3bb747e0aabb4
metehansenol <metehansenol@gmail.com>
2022-08-14 23:03:34 +0300

    generate content stream

--- index.js
+++ index.js
@@ -1,98 +1,88 @@
-#!/usr/bin/env node
-const program = require('commander');
-const fs = require('fs');
-const path = require('path');
+const Stream = require('stream');
 
-function write(stream, size) {
-
-  return new Promise(resolve => {
+function write(stream1, size) {
 
     let chunk = createChunk(size);
 
-    stream.write(chunk, () => {
+    stream1.write(chunk);
 
-      chunk = null;
-      console.log(`Wrote ${size} bytes...`);
-      resolve();
+    chunk = null;
 
-    });
-
-  });
+    console.log(`Wrote ${size} bytes...`);
 
 }
 
-async function writeMany(stream, size, times) {
+async function writeMany(stream1, size, times) {
 
-  for ( let i = 0; i < times; i++ ) {
+    for ( let i = 0; i < times; i++ ) {
 
-    console.log(`Writing chunk ${i+1}/${times}`);
-    await write(stream, size);
+        console.log(`Writing chunk ${i+1}/${times}`);
+        await write(stream1, size);
 
-  }
-
-  return;
+    }
 
 }
 
 function createChunk(size) {
 
-  const charset = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890';
-  let chunk = '';
+    const charset = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890';
+    let chunk = '';
 
-  for ( let i = 0; i < size; i++ ) {
+    for ( let i = 0; i < size; i++ ) {
+        chunk += charset[Math.floor(Math.random() * charset.length)];
+    }
 
-    chunk += charset[Math.floor(Math.random() * charset.length)];
-
-  }
-
-  return chunk;
+    return chunk;
 
 }
 
-program
-  .usage('<filename> <size>')
-  .action((filename, size) => {
+function getChunkSize(size) {
 
-    if ( typeof filename !== 'string' || typeof size !== 'string' ) {
+    let chunkSize = Math.floor(size / 10);
 
-      console.log('Both filename and size must be provided!');
-      process.exit(1);
+    if (!chunkSize) chunkSize = 256;
+    if (chunkSize > 10 * 1000 * 1000) chunkSize = 10 * 1000 * 1000;
 
-    }
+    return chunkSize;
+
+}
+
+async function generateDummyContent(size) {
 
     const bMatch = size.match(/^(\d+)$/i);
     const kbMatch = size.match(/^(\d+)kb$/i);
     const mbMatch = size.match(/^(\d+)mb/i);
     const gbMatch = size.match(/^(\d+)gb/i);
-    const filepath = path.join('.', filename);
-    let chunkSize = 0;
 
-    if ( bMatch ) size = +bMatch[1];
-    if ( kbMatch ) size = +kbMatch[1] * 1000;
-    if ( mbMatch ) size = +mbMatch[1] * 1000 * 1000;
-    if ( gbMatch ) size = +gbMatch[1] * 1000 * 1000 * 1000;
+    if (bMatch) size = +bMatch[1];
+    if (kbMatch) size = +kbMatch[1] * 1000;
+    if (mbMatch) size = +mbMatch[1] * 1000 * 1000;
+    if (gbMatch) size = +gbMatch[1] * 1000 * 1000 * 1000;
 
-    chunkSize = Math.floor(size / 10);
+    const stream = new Stream.PassThrough();
 
-    if ( ! chunkSize ) chunkSize = 256;
-    if ( chunkSize > 10 * 1000 * 1000 ) chunkSize = 10 * 1000 * 1000;
-
-    const stream = fs.createWriteStream(filepath, { highWaterMark: chunkSize });
+    const chunkSize = getChunkSize(size);
 
     const rounds = Math.floor(size / chunkSize);
     const lastRound = size % chunkSize;
 
     writeMany(stream, chunkSize, rounds)
-    .then(() => {
+        .then(() => {
 
-      stream.end(createChunk(lastRound), () => {
+            stream.end(createChunk(lastRound), () => {
 
-        console.log('File has been generated.');
+            console.log('File has been generated.');
 
-      });
+        });
 
     })
     .catch(console.log);
 
-  })
-  .parse(process.argv);
+    return stream;
+
+}
+
+module.exports = {
+    getChunkSize,
+    generateDummyContent,
+};
